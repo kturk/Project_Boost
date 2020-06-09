f3eda18b23010434732040c9ee628f30e7d2d7a9
Kemal Turk <kemalturk1997@gmail.com>
2020-06-09 03:26:15 +0300

    Added engine sound

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource engineAudio;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        engineAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.Space))
+        {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!engineAudio.isPlaying)
+                engineAudio.Play();
+        }
+        else 
+        {
+            engineAudio.Stop();
+        }
 
         if (Input.GetKey(KeyCode.A))
             transform.Rotate(Vector3.forward);
