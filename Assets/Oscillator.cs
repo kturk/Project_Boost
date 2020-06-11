8a12973ecd0c3ea5c6dbb77110234cf79f818d3b
Kemal Turk <okulmavi@gmail.com>
2020-06-11 21:39:13 +0300

    Oscillator script created (Objects can move in the game scene)

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float period;
+    //[Range(0, 1)] [SerializeField] 
+    float movementMultiplier;
+    Vector3 startingPosition;
+    private const float tau = Mathf.PI * 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+        movementVector = new Vector3(10f, 5f, 0f);
+        period = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period;
+        movementMultiplier = Mathf.Sin(cycles * tau) / 2f + 0.5f; // Between 0 and 1
+        Vector3 offset = movementMultiplier * movementVector;
+        transform.position = startingPosition + offset;
+    }
+}
